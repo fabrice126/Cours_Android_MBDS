32b12d5d98f9e7cb1f08eb93b47dafd0ef77825e
Thibaut Linares <thibaut.linares@laposte.net>
2015-10-22 15:04:12 +0200

    Page accueil
    accueil

--- app/src/main/java/miage/mbds/cours_mbds/MainActivity.java
+++ app/src/main/java/miage/mbds/cours_mbds/MainActivity.java
@@ -1,13 +1,14 @@
 package miage.mbds.cours_mbds;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("login", "Thibaut" );
+        intent.putExtra("password", "pass" );
+        startActivity(intent);
     }
 
     @Override
