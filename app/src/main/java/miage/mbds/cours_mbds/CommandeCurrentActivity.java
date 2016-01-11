6f2d8ee8f0e813f9feb1aec238ab40cdd2ecc387
Fabrice <fjauvat@gmail.com>
2016-01-11 15:52:29 +0100

    delete button float

--- app/src/main/java/miage/mbds/cours_mbds/CommandeCurrentActivity.java
+++ app/src/main/java/miage/mbds/cours_mbds/CommandeCurrentActivity.java
@@ -32,14 +32,7 @@ public class CommandeCurrentActivity extends AppCompatActivity implements Result
 
         e.async_list_menu(aq, this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
